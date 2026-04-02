27a22731f2914aebd132afe453da6e64c7633808
ddzinncmxt <j58191128@gmail.com>
2026-04-02 20:57:25 -0300

    inicializa o repositório

---
+++ atividade01.py
@@ -0,0 +1,17 @@
+# numero = int(input('Número:'))
+# n1 = 1
+# anterior = numero - n1
+# proximo = numero + n1
+# print(f'O seu número é {numero},seu antecessor é {anterior} e seu sucessor é {proximo}')
+
+salario = float(input('Salário: '))
+porcentagem = salario * 15 / 100
+valortotal = salario + porcentagem
+print(f'Valor final: {valortotal}')
+
+
+
+
+
+
+
---
+++ exemplo_01.py
@@ -0,0 +1,66 @@
+# print(f'ola mundo')
+# nome = "joao"
+# idade = 18
+#  # nomes utilizam "" e palavras tambem
+# print(f'olamundo me chamo {nome} e tenho {idade} anos')
+# print('e comecei a fazer um curso fd lek')
+# cidade = "niteroi"
+# print(f'fica na cidade de {cidade}')
+# # -------------------------------------------------------------------------------------------------------------------------------
+# # atividade de fixação
+# nome = "eduardo"
+# profissao = "atleta"
+
+# bairro = "tribobo"
+# print(f'nome:{nome}')
+# print(f'profissão:{profissao}')
+# print(f'bairro:{bairro}')
+#----------------------------------------------------------- operadores ----------------------------------------------------------------------------------
+# preco = 20 
+# quantidade = 3 
+# total = preco * quantidade
+# print(f'O total é {total}')
+#--------------------------------------------------- atividade de fixação---------------------------------------------------------------------
+
+# bonus = 880
+# salario = 1500
+# total = 1500 + 880
+# print(f'Seu salario desse mês é {total}')
+# ----------------------------------------------------------------------------------------------------------------------
+# Exemplo de entrada de valores pelo usuario
+# nome = input('Informe o nome:')
+# print(f'Olá {nome}!')
+# idade = input('Informe a idade:')
+# print(f'{idade}')
+
+# salario = float(input('Informe seu salário:'))
+# bonus = float(input('informe o bônus:'))
+# total = salario + bonus
+# print(f'Seu salário total é de :{total}')
+# #variaves de numero se for numero quebrado(casa decimal) tipo salario coloca float(input(              ))
+# #int é para inteiros
+# #string = texto
+# #float = Números decimais
+# #int = Número inteiro
+#__________________________________________________________________________________________________calculo de porcentagem
+# preco = float(input('Informe o preço: '))
+# quantidade = int(input('informe a quantidade: '))
+# total = preco * quantidade
+# desconto = total * 10 / 100
+# valorfinal = total - desconto
+# print(f'Valor a pagar:{valorfinal}')
+ 
+salario = float(input('Informe seu salário: '))
+desconto = salario * 35 / 100
+total = salario - desconto
+print(f'Valor total: {total}')
+
+
+
+
+
+
+
+
+
+
