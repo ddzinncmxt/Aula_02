2dd2de1a994fe7d88f34facee3100ae3906f2481
ddzinncmxt <j58191128@gmail.com>
2026-04-02 21:37:53 -0300

    atividade média dos alunos

---
+++ atividade03.py
@@ -0,0 +1,9 @@
+nome = input('Informe o nome: ')
+pbimestrenota = float(input('Nota 1: '))
+sbimestrenota = float(input('Nota 2: '))
+tbimestrenota = float(input('Nota 3: '))
+qbimestrenota = float(input('Nota 4: '))
+media = (pbimestrenota + sbimestrenota + tbimestrenota + qbimestrenota) / 4
+print(f'Média: {media}')
+
+
